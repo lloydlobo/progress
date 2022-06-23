001b8433f82ec000a3bc1bc883f06ac6fed08d82
lloydlobo <lloyd@lloydlobo.com>
2022-06-23 20:48:08 +0530

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,43 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true,
+
+			scss: {
+				prependData: '@use "src/variables.scss" as *;'
+			}
+		}),
+		mdsvex(mdsvexConfig)
+	],
+
+	kit: {
+		adapter: adapter(),
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		},
+
+		vite: {
+			css: {
+				preprocessorOptions: {
+					scss: {
+						additionalData: '@use "src/variables.scss" as *;'
+					}
+				}
+			}
+		}
+	}
+};
+
+export default config;
